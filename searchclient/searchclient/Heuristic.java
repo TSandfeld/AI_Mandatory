168d8b9a7d6ef52a78c9592e5b5fed36653fc966
TSandfeldNielsen <thomas.sandfeld@gmail.com>
2018-02-07 12:57:20 +0100

    Start exercise 4

--- searchclient/searchclient/Heuristic.java
+++ searchclient/searchclient/Heuristic.java
@@ -1,15 +1,63 @@
 package searchclient;
 
 import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
 
 import searchclient.NotImplementedException;
 
+class Tuple<X,Y> {
+	public final X x;
+	public final Y y;
+	public Tuple(X x, Y y) {
+		this.x = x;
+		this.y = y;
+	}
+}
+
 public abstract class Heuristic implements Comparator<Node> {
+	HashMap<Character, Tuple<Integer, Integer>> goalPositions = new HashMap<>();
+	HashMap<Character, Tuple<Integer, Integer>> boxPositions = new HashMap<>();
+
 	public Heuristic(Node initialState) {
 		// Here's a chance to pre-process the static parts of the level.
+		findPositions(initialState);
+	}
+
+	public void findPositions(Node n) {
+		if (!this.goalPositions.isEmpty()) {
+			this.goalPositions = new HashMap<>();
+			this.boxPositions = new HashMap<>();
+		}
+
+		char[][] goals = n.goals;
+		char[][] boxes = n.boxes;
+
+		for (int row = 0; row < n.MAX_ROW; row++) {
+			for (int col = 0; col < n.MAX_COL; col++) {
+				char goalChr = goals[row][col];
+				char boxChr = boxes[row][col];
+
+				if ('a' <= goalChr && goalChr <= 'z') { // goal
+					this.goalPositions.put(goalChr, new Tuple<>(row, col));
+				}
+
+				if ('A' <= boxChr && boxChr <= 'Z') {
+					this.boxPositions.put(boxChr, new Tuple<>(row, col));
+				}
+			}
+		}
 	}
 
 	public int h(Node n) {
+		// Heurstics: Straight-line
+		findPositions(n);
+
+		int agentRow = n.agentRow;
+		int agentCol = n.agentCol;
+
+		
+
 		throw new NotImplementedException();
 	}
 
